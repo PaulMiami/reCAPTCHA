203872f1e05ca11f2037c391bbb4e0d82f96fe35
Paul Biccherai <pbiccherai@hotmail.com>
2016-06-23 14:53:38 -0400

    Creating project

---
+++ src/PaulMiami.AspNetCore.Mvc.Recaptcha/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PaulMiami.AspNetCore.Mvc.Recaptcha")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d8c6e1d-34f8-4e50-af57-24991161d283")]
