1ac762c4997ee2e03b90b48a530df074f92ad09f
PaulMiami <pbiccherai@hotmail.com>
2020-01-19 21:25:13 -0500

    Fix coverage

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,3 +1,47 @@
-$testProjectLocations = @('test/PaulMiami.AspNetCore.Mvc.Recaptcha.Test')
+$ErrorActionPreference = "Stop"
+
 $outputLocation = 'testResults'
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/PaulMiami/BuildTools/4cc3b77556ec820f96af8a69ef86959a8abce2c1/Coverage.ps1'))
+
+$outputPath = (Resolve-Path $outputLocation).Path
+$outputFile = Join-Path $outputPath -childpath 'coverage.xml'
+
+Write-Output $outputPath
+Write-Output $outputFile
+
+Function Verify-OnlyOnePackage
+{
+	param ($name)
+
+	$location = $env:USERPROFILE + '\.nuget\packages\' + $name
+	If ((Get-ChildItem $location).Count -ne 1)
+	{
+		throw 'Invalid number of packages installed at ' + $location
+	}
+}
+
+Verify-OnlyOnePackage 'coveralls.io'
+Verify-OnlyOnePackage 'ReportGenerator'
+
+pushd
+Try
+{
+	# Either display or publish the results
+	If ($env:CI -eq 'True')
+	{
+		$command = (Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\coveralls.io'))[0].FullName + '\tools\coveralls.net.exe' + ' --opencover "' + $outputFile + '" --full-sources'
+		Write-Output $command
+		iex $command
+	}
+	Else
+	{
+		$command = (Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\ReportGenerator'))[0].FullName + '\tools\net47\ReportGenerator.exe -reports:"' + $outputFile + '" -targetdir:"' + $outputPath + '"'
+		Write-Output $command
+		iex $command
+		cd $outputPath
+		./index.htm
+	}
+}
+Finally
+{
+	popd
+}
